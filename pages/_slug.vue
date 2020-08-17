d5f3b19139ab3dedd5976a73a860e86f8091813a
oussamah <oussamah.jaber@gmail.com>
2020-08-17 11:44:59 +0200

    feat: go full static

---
+++ pages/_slug.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <h1>
+      {{ mountain.title }}
+    </h1>
+    <h3>
+      {{ mountain.continent }}
+    </h3>
+    <img :src="mountain.image" :alt="mountain.title" />
+    <p>{{ mountain.description }}</p>
+    <nuxt-link to="/">Nack to the list</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData({ params }) {
+    const mountain = fetch(
+      `https://api.nuxtjs.dev/mountains/${params.slug}`
+    ).then(res => res.json());
+    return { mountain };
+  }
+};
+</script>
+
+<style>
+img {
+  max-width: 100%;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Beautiful mountains</h1>
+    <ul>
+      <li v-for="mountain in mountains" :key="mountain.slug">
+        <nuxt-link :to="mountain.slug"> {{ mountain.title }}</nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData() {
+    const mountains = fetch("https://api.nuxtjs.dev/mountains").then(
+      res => res.json()
+    );
+    return { mountains };
+  }
+};
+</script>
